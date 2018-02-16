d592ecd4780c039978a3491e779971779b3ba3f8
Daniel <clancy.daniel@gmail.com>
2018-02-15 20:14:51 -0600

    first commit

---
+++ making_dictionaries.py
@@ -0,0 +1,12 @@
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+def make_dict(list1, list2):
+	if len(list1) > len(list2):
+		new_dict = dict(zip(list1, list2))
+	else:
+		new_dict = dict(zip(list2, list1))
+  
+	return new_dict
+
+print make_dict(name, favorite_animal)
